5ad9cb22ba3eeffe55f602dcf934b382af0d62a2
nabiilafath <72002975+nabiilafath@users.noreply.github.com>
2022-09-21 12:35:09 +0700

    Add files via upload

---
+++ beasiswa_saw/page/home.php
@@ -0,0 +1,11 @@
+<body bgcolor="Pink">
+	<h1>
+		<p align="center">
+			<font color="maroon">
+				SISTEM PENDUKUNG KEPUTUSAN <br>
+				Penentuan Penerima Beasiswa LAZISWAF di Universitas Darussalam Gontor <br>
+				Menggunakan Metode Simple Additive Weighting (SAW)
+	</h1>
+	</b>
+	</p>
+	</font>
